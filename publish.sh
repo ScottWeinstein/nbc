2b935fe1f6ea10d8fb909da53b76d70d3434e852
Scott Weinstein <scottwww@squidnet.com>
2013-02-28 00:12:06 -0500

    fix publish script

--- publish.sh
+++ publish.sh
@@ -4,9 +4,12 @@ grunt
 git checkout gh-pages
 mv output/* .
 rm -r output
+
+rm -rf assets
 rm doc/index.html
-mv doc/lib doc/doc_lib
-mv doc/*
+mv doc/lib doc_lib
+mv doc/* .
 git add -A
 git commit -m "update docs"
 git push 
+g checkout master
