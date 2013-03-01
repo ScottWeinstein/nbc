9dc7a0fba07464f3a1931ce252195e3ef236f8b6
Scott Weinstein <scottwww@squidnet.com>
2013-02-28 19:59:48 -0500

    update docs

--- assets/behavior.js
+++ assets/behavior.js
@@ -90,9 +90,43 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "title": "nbc"
           },
           "depth": 2,
-          "outline": []
+          "outline": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 1,
+                "title": "Training",
+                "slug": "training"
+              },
+              "depth": 1
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 1,
+                "title": "Classification",
+                "slug": "classification"
+              },
+              "depth": 1
+            }
+          ]
         }
       ]
+    }, {
+      "type": "file",
+      "data": {
+        "language": {
+          "nameMatchers": [".coffee", "Cakefile"],
+          "pygmentsLexer": "coffee-script",
+          "singleLineComment": ["#"],
+          "name": "CoffeeScript"
+        },
+        "sourcePath": "/Users/sweinstein/d/github/nbc/test.coffee",
+        "projectPath": "test.coffee",
+        "targetPath": "test",
+        "title": "test"
+      },
+      "depth": 1,
+      "outline": []
     }
   ];
 
